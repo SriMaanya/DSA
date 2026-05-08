5c436e1e491f85535ef570e9ebe7e781f0d133c3
SriMaanya <rapolusrimaanya10@gmail.com>
2026-05-08 22:31:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.cpp
@@ -0,0 +1,38 @@
+class Solution {
+  public:
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        // code here
+        int n = a.size(),m = b.size();
+        int i=0,j=0;
+        vector<int> unionArr;
+        
+        while(i<n && j<m){
+            
+            if(a[i]<=b[j]){
+                if(unionArr.size() == 0 || unionArr.back() != a[i])
+                    unionArr.push_back(a[i]);
+                i++;
+            }
+            
+            else{
+                if(unionArr.size() == 0 || unionArr.back() != b[j])
+                    unionArr.push_back(b[j]);
+                j++;
+            }
+        }
+        
+        while(i<n){
+            if(unionArr.size() == 0 || unionArr.back() != a[i])
+                    unionArr.push_back(a[i]);
+            i++;
+        }
+        
+        while(j<m){
+            if(unionArr.size() == 0 || unionArr.back() != b[j])
+                    unionArr.push_back(b[j]);
+            j++;
+        }
+        
+        return unionArr;
+    }
+};
