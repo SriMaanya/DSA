edb42d67a530529b0263972ce16552ccb38e362e
SriMaanya <rapolusrimaanya10@gmail.com>
2026-05-14 20:26:54 +0530

    Time: 104 ms (8.38%) | Memory: 92.3 MB (22.00%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int,int> mpp;
+        for(int i=0;i<nums.size();i++){
+            if(mpp.find(nums[i]) != mpp.end())
+                return true;
+            mpp[nums[i]] = i;
+        }
+        return false;
+    }
+};
