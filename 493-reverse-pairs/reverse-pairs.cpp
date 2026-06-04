7bae78c1b90ca054e14afa4abadd7f35efe72d41
SriMaanya <rapolusrimaanya10@gmail.com>
2026-06-04 06:34:15 +0530

    Time: 478 ms (68.13%) | Memory: 243.5 MB (34.62%) - LeetSync

---
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -0,0 +1,67 @@
+void merge(vector<int>& arr, int low, int mid, int high) {
+        // Create temp arrays
+        vector<int> temp;
+        int left = low, right = mid + 1;
+
+        // Merge two sorted halves
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right])
+                temp.push_back(arr[left++]);
+            else
+                temp.push_back(arr[right++]);
+        }
+
+        // Copy remaining elements from left half
+        while (left <= mid)
+            temp.push_back(arr[left++]);
+
+        // Copy remaining elements from right half
+        while (right <= high)
+            temp.push_back(arr[right++]);
+
+        // Copy sorted elements back to original array
+        for (int i = low; i <= high; i++)
+            arr[i] = temp[i - low];
+}
+
+int countPairs(vector<int> &arr, int low, int mid, int high){
+
+    int right = mid + 1;
+    int cnt = 0;
+    for(int i = low; i<=mid;i++){
+        while(right <= high && arr[i] > 2L * arr[right]) right++;
+        cnt += (right - (mid + 1));
+    }
+    return cnt;
+}
+
+    // Recursive merge sort function
+int mergeSort(vector<int>& arr, int low, int high) {
+    int cnt = 0;
+    if (low >= high)
+        return cnt;
+    // Find the middle index
+    int mid = (low + high) / 2;
+
+    // Recursively sort left half
+    cnt +=mergeSort(arr, low, mid);
+
+    // Recursively sort right half
+    cnt +=mergeSort(arr, mid + 1, high);
+
+    cnt += countPairs(arr,low,mid,high);
+
+    // Merge the two sorted halves
+    merge(arr, low, mid, high);
+
+    return cnt;
+}
+
+class Solution {
+public:
+    int reversePairs(vector<int>& nums) {
+        int cnt;
+        cnt = mergeSort(nums, 0 ,nums.size() - 1);
+        return cnt;
+    }
+};
